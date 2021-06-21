1ff179d3bcac2a8160fbea84e1b9d79f1d094920
ksairafian <77306531+ksairafian@users.noreply.github.com>
2021-06-21 12:12:30 -0500

    Update Surgerychecker.py

--- Surgerychecker.py
+++ Surgerychecker.py
@@ -1,44 +1,50 @@
 import smtplib
 import ssl
 import bs4
-import certifi
 import requests
-import urllib3
+import schedule
+import time
 
-
-def getsurgerydate(url):
+# Scrapes the APDS website for the latest job posting
+def getsurgdate(url):
+    # APDS website TLS certificate is not acquirable automatically - must link to certificate in a txt file
     res = requests.get(url, verify='/Users/kgs44/PycharmProjects/tkinterpractice/certificate.txt')
     res.raise_for_status()
 
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    elems = soup.select('#filter-list > div:nth-child(1) > a > div.card-block.m-l-auto.text-muted.item-flex.align-items-flex-end > span')
-    return elems[0].text.strip()
+    elems = soup.select('#filter-list > div:nth-child(1) > a')
+    return elems[0].text.split()
 
-class Mail:
+# Scrapes the APDR website for latest radiology job posting date
+def getradsdate(url):
+    res = requests.get(url)
+    res.raise_for_status()
 
-    def __init__(self):
-        self.port = 465
-        self.smtp_server_domain_name = "smtp.gmail.com"
-        self.sender_mail = "<Desired Email>"
-        self.password = "<Password>"
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.select('#content > div > div > div.col > div:nth-child(4) > div > div > div > p:nth-child(3) > strong')
+    return elems[0].text.split()
 
-    def send(self, emails, subject, content):
-        ssl_context = ssl.create_default_context()
-        service = smtplib.SMTP_SSL(self.smtp_server_domain_name, self.port, context=ssl_context)
-        service.login(self.sender_mail, self.password)
+surg_text = getsurgdate('https://apds.org/education-careers/open-positions/')
+rads_text = getradsdate('https://www.apdr.org/trainees/residency-information/residency-positions-available')
 
-        for email in emails:
-            result = service.sendmail(self.sender_mail, email, f"Subject: {subject}\n{content}")
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "<desired email address to send emails>"
+receiver_email = "<email address to receive emails>"
+password = "<password for sender email>"
+message = f"""\
+Subject: Latest Job Postings
 
-        service.quit()
 
+Latest APDS Post: {surg_text} \n
+Latest APDR post: {rads_text}
+"""
 
-if __name__ == '__main__':
-    mails = input("Enter emails: ").split()
-    subject = input("Enter subject: Latest APDS Post")
-    text = getsurgerydate('https://apds.org/education-careers/open-positions/')
-    content = input("Enter content: " + f'{text}')
-
-    mail = Mail()
-    mail.send(mails, subject, content)
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
 
